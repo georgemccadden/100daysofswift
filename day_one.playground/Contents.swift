4054cd58f28a31ec3eadfb54992a9400c75c91e2
@georgemccadden <thethirdone.g@gmail.com>
2021-10-10 23:37:55 -0700

    A few additional things learned

---
+++ day_one.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var greeting = "Welcome to the first day of the SwiftUI challenge!"
+
+var name = "George"
+
+
+greeting = """
+Welcome to the SwiftUI challenge, George.
+
+I'm so excited that you're embarking on this journey!
+"""
+
+print(greeting)
+
+var doYouLikeToCode = false
+
+doYouLikeToCode.toggle()
+
+print(doYouLikeToCode)
